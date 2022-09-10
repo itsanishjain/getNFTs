2166b07405d17579ec5ae4149012061060892359
Anish <helloanishjain@gmail.com>
2022-09-10 21:21:57 +0530

    add css for modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,9 +10,8 @@ const Modal = ({ setIsModalOpen, currentItem }) => {
       aria-modal="true"
     >
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
-      <div className="fixed inset-0 z-10 overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      <div className="bg-red-500 fixed inset-0 z-10 overflow-y-auto">
+        {/* <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
@@ -37,6 +36,35 @@ const Modal = ({ setIsModalOpen, currentItem }) => {
                       your data will be permanently removed. This action cannot
                       be undone.
                     </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button
+                onClick={() => setIsModalOpen(false)}
+                type="button"
+                className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div> */}
+
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="sm:flex sm:items-start">
+                <div className="flex bg-red-100 p-2">
+                  <Image
+                    src={currentItem.media[0].raw}
+                    height={250}
+                    width={400}
+                    alt=""
+                  />
+                  <div className="text-black p-2">
+                    #{parseInt(currentItem.id.tokenId)}
                   </div>
                 </div>
               </div>
