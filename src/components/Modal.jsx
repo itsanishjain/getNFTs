df375be5192ffb4873286420e887a1a7246aeaf6
Anish <helloanishjain@gmail.com>
2022-09-11 11:32:56 +0530

    add responsive and gradient for modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -46,7 +46,7 @@ const Modal = ({ setIsModalOpen, currentItem }) => {
               <button
                 onClick={() => setIsModalOpen(false)}
                 type="button"
-                className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
               >
                 Cancel
               </button>
@@ -55,22 +55,26 @@ const Modal = ({ setIsModalOpen, currentItem }) => {
         </div> */}
 
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div className="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg bg-gradient-to-r from-slate-600 to-slate-800">
+            <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
-                <div className="flex bg-red-100 p-2">
+                <div className="flex flex-col justify-center items-center p-2">
                   <Box item={currentItem} />
-                  <div className="text-black p-2">
+                  <div className="text-gray-200 p-2">
                     #{parseInt(currentItem.id.tokenId)}
+                    {/* <div>{currentItem?.contractMetadata.name}</div> */}
+                    <div className="mt-2 text-sm md:text-lg">
+                      {currentItem.description}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 bg-gradient-to-r from-slate-600 to-slate-800">
               <button
                 onClick={() => setIsModalOpen(false)}
                 type="button"
-                className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-slate-900 border-none  px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
               >
                 Cancel
               </button>
